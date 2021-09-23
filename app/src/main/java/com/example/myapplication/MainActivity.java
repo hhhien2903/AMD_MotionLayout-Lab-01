8a3e9076b445e9220f471ea21ca7a9fb837bc166
mrbin2903 <hhhien2903@gmail.com>
2021-09-23 22:12:59 +0700

    done ex3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BabyActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ex2);
+//        setContentView(R.layout.activity_ex1);
+//        setContentView(R.layout.activity_ex2);
+        setContentView(R.layout.activity_ex3);
     }
 }
